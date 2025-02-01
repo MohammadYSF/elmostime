780ab75c97a696906858cf84bd0d6288956a3a34
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-02-01 19:35:42 +0330

    do some

--- src/App.svelte
+++ src/App.svelte
@@ -47,74 +47,205 @@
 </style> -->
 <script>
 	import bgImage from "./assets/bg.jpg";
-	let w = 40
+	let w = 45;
 </script>
 
-<div>
+<div class="background" style="background-image: url({bgImage});">
 	<table class="w-full" style="table-layout: fixed;">
 		<thead>
 			<tr>
 				<th style="width:{w}px;" class="border-b text-left">#</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">7</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >7:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l"  >8</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >8:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l"  >9</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >9:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">10</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >10:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">11</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >11:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">12</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >12:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">13</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >13:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">14</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >14:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">15</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >15:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">16</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >16:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">17</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >17:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">18</th>
-				<th style="width:{w}px;" class="border-b text-left invisible"  >18:30</th>
-				<th style="width:{w}px;" class="border-b text-left border-l">19</th>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>7</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>7:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>8</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>8:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>9</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>9:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>10</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>10:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>11</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>11:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>12</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>12:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>13</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>13:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>14</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>14:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>15</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>15:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>16</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>16:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>17</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>17:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>18</th
+				>
+				<th style="width:{w}px;" class="border-b text-left invisible"
+					>18:30</th
+				>
+				<th style="width:{w}px;" class="border-b text-left border-l"
+					>19</th
+				>
 			</tr>
 		</thead>
 		<tbody>
 			{#each Array(6) as _, index}
 				<tr>
-					<td style="height: 50px; width: {w}px" class="border-b">D{index+1}</td>  <!--#-->
-					<td style="height: 50px; width: {w}px" class="border-l border-b">07:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b bg-green-500" colspan="3">آمار و احتمال مهندسی</td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b hidden">08:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b hidden"></td>
-					<td style="height: 50px; width: {w}px"class="border-l border-b" colspan="3">
-						<div class="bg-red-700">معادلات دیفرانسیل</div>
-						<div class="bg-red-300">جبرخطی عددی</div>
-						<div class="bg-red-500">بهینه سازی غیرخطی</div>
+					<td style="height: 50px; width: {w}px" class="border-b"
+						>D{index + 1}</td
+					>
+					<!--#-->
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">07:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b">
+						<div
+							class="inline-block bg-green-700 z-20 relative"
+							style="width: {3 * w}px;"
+						>
+							آمار و احتمال مهندسی
+						</div>
+					</td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b"
+					></td>
+					<td style="height: 50px; width: {w}px" class="border-b">
+						<div
+							class="bg-green-700 inline-block z-10 relative"
+							style="width: {3 * w}px"
+						>
+							معادلات دیفرانسیل
+						</div>
+						<div
+							class="bg-green-700 inline-block z-10 relative"
+							style="width: {3 * w}px"
+						>
+							جبرخطی عددی
+						</div>
+						<div
+							class="bg-green-700 inline-block z-10 relative"
+							style="width: {2 * w}px"
+						>
+							بهینه سازی غیرخطی
+						</div>
+						<div
+							class="bg-green-700 inline-block z-10 relative"
+							style="width: {2 * w}px"
+						>
+							بهینه سازی غیرخطی
+						</div>
+					</td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b"
+					>
 					</td><!--9-->
-					<td style="height: 50px; width: {w}px" class="border-b hidden"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b hidden">10:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">11:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">12:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">13:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">14:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">15:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">16:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">17:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">18:00</td>
-					<td style="height: 50px; width: {w}px" class="border-b"></td>
-					<td style="height: 50px; width: {w}px" class="border-l border-b">19:00</td>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">10:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">11:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">12:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">13:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">14:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">15:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">16:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">17:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">18:00</td
+					>
+					<td style="height: 50px; width: {w}px" class="border-b"
+					></td>
+					<td
+						style="height: 50px; width: {w}px"
+						class="border-l border-b">19:00</td
+					>
 				</tr>
 			{/each}
 		</tbody>
@@ -130,4 +261,5 @@
 	.overlay {
 		background-color: rgba(0, 0, 0, 0.6);
 	}
+
 </style>
