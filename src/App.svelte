ad36e20a32ad8ee1dd70aa6b3bb7c1859a340418
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-02-01 20:15:57 +0330

    ok setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,251 +1,139 @@
-<!-- <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style> -->
 <script>
 	import bgImage from "./assets/bg.jpg";
+	import axios from "axios";
+	import { onMount } from "svelte";
+
+	let flat_data = [];
+	let error = "NO_ERROR";
+	let selected_courses = ["1211011_01", "1711014_03", "2011192_01","1611042_01"];
+	async function fetch_data() {
+		try {
+			const response = await axios.get("http://127.0.0.1:5000/data");
+			flat_data = Object.entries(response.data).flatMap(
+				([category, courses]) =>
+					courses.map((course) => ({ ...course, category })),
+			);
+			set_filtered_data();
+			console.log(flat_data);
+		} catch (err) {
+			error = err.message;
+		}
+	}
 	let w = 45;
+	fetch_data();
+
+	const TIMES = [
+		"07:00",
+		"07:30",
+		"08:00",
+		"08:30",
+		"09:00",
+		"09:30",
+		"10:00",
+		"10:30",
+		"11:00",
+		"11:30",
+		"12:00",
+		"12:30",
+		"13:00",
+		"13:30",
+		"14:00",
+		"14:30",
+		"15:00",
+		"15:30",
+		"16:00",
+		"16:30",
+		"17:00",
+		"17:30",
+		"18:00",
+		"18:30",
+		"19:00",
+	];
+	const WEEK_DAYS = [
+		"SATURDAY",
+		"SUNDAY",
+		"MONDAY",
+		"TUESDAY",
+		"WEDNESDAY",
+		"THURSDAY",
+	];
+	const PERSIAN_WEEKDAY_MAP = new Map();
+	PERSIAN_WEEKDAY_MAP.set("SATURDAY", "شنبه");
+	PERSIAN_WEEKDAY_MAP.set("SUNDAY", "یکشنبه");
+	PERSIAN_WEEKDAY_MAP.set("MONDAY", "دوشنبه");
+	PERSIAN_WEEKDAY_MAP.set("TUESDAY", "سه شنبه");
+	PERSIAN_WEEKDAY_MAP.set("WEDNESDAY", "چهارشنبه");
+	PERSIAN_WEEKDAY_MAP.set("THURSDAY", "پنجشنبه");
+	let filtered_data = [];
+	function set_filtered_data() {
+		filtered_data = flat_data.filter(
+			(a) =>
+				selected_courses.findIndex(
+					(b) => b == a.course_number_and_group,
+				) != -1,
+		);
+	}
+
+	onMount(fetch_data);
+	// console.log("data", flat_data);
+	// console.log("fdata", filtered_data);
 </script>
 
 <div class="background" style="background-image: url({bgImage});">
 	<table class="w-full" style="table-layout: fixed;">
 		<thead>
 			<tr>
-				<th style="width:{w}px;" class="border-b text-left">#</th>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>7</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>7:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>8</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>8:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>9</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>9:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>10</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>10:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>11</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>11:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>12</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>12:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>13</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>13:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>14</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>14:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>15</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>15:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>16</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>16:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>17</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>17:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>18</th
-				>
-				<th style="width:{w}px;" class="border-b text-left invisible"
-					>18:30</th
-				>
-				<th style="width:{w}px;" class="border-b text-left border-l"
-					>19</th
-				>
+				<th style="width:{w + 40}px;" class="border-b text-left">#</th>
+				{#each TIMES as t, i}
+					{#if i % 2 == 0}
+						<th
+							style="width:{w}px;"
+							class="border-b text-left border-l"
+							>{t.substring(0, 2)}</th
+						>
+					{:else}
+						<th
+							style="width:{w}px;"
+							class="border-b text-left invisible">{t}</th
+						>
+					{/if}
+
+				{/each}
 			</tr>
 		</thead>
 		<tbody>
-			{#each Array(6) as _, index}
+			{#each WEEK_DAYS as item, _}
 				<tr>
-					<td style="height: 50px; width: {w}px" class="border-b"
-						>D{index + 1}</td
+					<td style="height: 50px;" class="border-b"
+						>{PERSIAN_WEEKDAY_MAP.get(item)}</td
 					>
-					<!--#-->
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">07:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b">
-						<div
-							class="inline-block bg-green-700 z-20 relative"
-							style="width: {3 * w}px;"
-						>
-							آمار و احتمال مهندسی
-						</div>
-					</td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b"
-					></td>
-					<td style="height: 50px; width: {w}px" class="border-b">
-						<div
-							class="bg-green-700 inline-block z-10 relative"
-							style="width: {3 * w}px"
-						>
-							معادلات دیفرانسیل
-						</div>
-						<div
-							class="bg-green-700 inline-block z-10 relative"
-							style="width: {3 * w}px"
-						>
-							جبرخطی عددی
-						</div>
-						<div
-							class="bg-green-700 inline-block z-10 relative"
-							style="width: {2 * w}px"
-						>
-							بهینه سازی غیرخطی
-						</div>
-						<div
-							class="bg-green-700 inline-block z-10 relative"
-							style="width: {2 * w}px"
-						>
-							بهینه سازی غیرخطی
-						</div>
-					</td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b"
-					>
-					</td><!--9-->
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">10:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">11:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">12:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">13:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">14:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">15:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">16:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">17:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">18:00</td
-					>
-					<td style="height: 50px; width: {w}px" class="border-b"
-					></td>
-					<td
-						style="height: 50px; width: {w}px"
-						class="border-l border-b">19:00</td
-					>
+					{#each TIMES as t, i}
+						{#if filtered_data.filter((a) => a.lecture_schedules.findIndex((b) => b.start_time == t && b.day_of_week == item) != -1).length > 0}
+							<td
+								style="height: 50px; width: {w}px"
+								class="border-b"
+							>
+								{#each filtered_data.filter((a) => a.lecture_schedules.findIndex((b) => b.start_time == t && b.day_of_week == item) != -1) as c, _}
+									<div
+										class="bg-green-700 inline-block z-10 relative"
+										style="width: {3 * w}px"
+									>
+										{c.course_name}
+									</div>
+								{/each}
+							</td>
+						{:else if i % 2 == 0}
+							<td
+								style="height: 50px; width: {w}px"
+								class="border-l border-b"
+							></td>
+						{:else}
+							<td
+								style="height: 50px; width: {w}px"
+								class="border-b"
+							></td>
+						{/if}
+					{/each}
 				</tr>
 			{/each}
 		</tbody>
@@ -261,5 +149,4 @@
 	.overlay {
 		background-color: rgba(0, 0, 0, 0.6);
 	}
-
 </style>
