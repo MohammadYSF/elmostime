f1d808846e7152c7c52be2ac2f60078b90419d18
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-02-07 13:49:27 +0330

    added Home page

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,19 @@
 	import { Route, Router, Link } from "svelte-routing";
 	import PrivateRoute from "./components/PrivateRoute.svelte";
 	import Login from "./components/Login.svelte";
-	import { authStore, login, logout, checkAuthentication } from "./authStore";
-	import { onMount } from "svelte";
+	import { authStore, logout, checkAuthentication } from "./authStore";
 	import TimeSchedules from "./TimeSchedules.svelte";
-    import Register from "./components/Register.svelte";
-    import Exams from "./Exams.svelte";
+	import Register from "./components/Register.svelte";
+	import Exams from "./Exams.svelte";
+	import Home from "./Home.svelte";
 	checkAuthentication();
 </script>
 
 <Router>
 	<nav class="fixed top-0 left-0 w-full bg-transparent shadow-md p-4 z-50">
 		<div class="flex space-x-4">
+			<Link to="/" class="text-gray-300 hover:text-green-600">خانه</Link>
 			{#if $authStore.isAuthenticated}
-				<Link to="/Home" class="text-gray-300 hover:text-green-600"
-					>خانه</Link
-				>
-
 				<Link to="/time" class="text-gray-300 hover:text-green-600"
 					>برنامه هفتگی</Link
 				>
@@ -26,21 +23,28 @@
 				>
 
 				<a
-					href="#"
+					role="button"
 					onclick={logout}
 					class="text-red-700 hover:text-red-400"
 				>
 					خروج
 				</a>
+			{:else}
+				<Link to="/login" class="text-gray-300 hover:text-green-600"
+					>ورود</Link
+				>
+				<Link to="/register" class="text-gray-300 hover:text-green-600"
+					>ثبت نام</Link
+				>
 			{/if}
 		</div>
 	</nav>
 
-	<main class="mt-5" style="max-width: 100%;">
-		<Route path="/login" component={Login} />
+	<main class="mt-10" style="max-width: 100%;">
 		<Route path="/register" component={Register} />
+		<Route path="/" component={Home} />
+		<Route path="/login" component={Login} />
 		<PrivateRoute path="/exams" component={Exams} />
 		<PrivateRoute path="/time" component={TimeSchedules} />
 	</main>
-
 </Router>
---
+++ src/Home.svelte
@@ -0,0 +1,34 @@
+<script>
+    import ELMOSTime_logo from "./assets/ELMOSTime_Persian.png";
+    import { Link } from "svelte-routing";
+</script>
+
+<div class="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg mx-auto" style="max-height: 100%;">
+    <img src={ELMOSTime_logo} />
+    <h3 class="text-xl font-semibold text-center text-gray-700 mb-6">
+        برای استفاده لطفا
+        <Link to="/login">وارد حساب خود شوید</Link>
+        یا
+        <Link to="/register">ثبت نام کنید</Link>         
+
+    </h3>
+    <p class="text-gray-500 text-right mb-2" dir="rtl">
+        ایده گرفته از 
+        <a href="https://elmos-vahed.ir">
+            علموص واحد
+        </a>
+    </p>
+    <p class="text-gray-500 text-right mb-2" dir="rtl">
+        با معرفی عملوص تایم به دوستان خود از ما حمایت کنید
+    </p>
+    <p class="text-gray-500 text-right mb-2" dir="rtl">
+        با ستاره دادن به مخزن گیتهاب
+        <a href="#">علموص تایم</a>
+        از ما حمایت کنید
+    </p>
+    <p class="text-gray-500 text-right" dir="rtl">
+        از طریق آدرس ایمیل
+        <a role="text">aa.yosefiyan7@gmail.com</a>
+        با ما صحبت کنید
+    </p>
+</div>
