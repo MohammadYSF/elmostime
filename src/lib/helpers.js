167fdfbbc207fb1ffe8a067c34fe5c32d2dcd839
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-02-14 21:48:25 +0330

    helper toPersianNumbers method bug fixed

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,10 +1,11 @@
 export function toPersianNumbers(num) {
+    if (num.toString().trim().length == 0) return "";
     const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
     return num.toString().replace(/\d/g, (d) => persianDigits[d]);
 }
 export function countHalfHours(startTime, endTime) {
-    console.log(startTime,"##",endTime);
-    
+    console.log(startTime, "##", endTime);
+
     // Convert HH:MM to total minutes
     function timeToMinutes(time) {
         const [hours, minutes] = time.split(":").map(Number);
